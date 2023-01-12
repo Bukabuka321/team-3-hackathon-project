89b5108a8ee87d99bd5a291f35d545a67eb2e2d3
OLGA DOBROVOLSKA <3250186@gmail.com>
2023-01-12 19:14:37 +0200

    added links and text in footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div>Wilders 2023</div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <div>
+        <ul className="footer_list">
+          <li className="item_footer">
+            <Link className="link_footer" href="/privacy">
+              Copyright & Privacy policy
+            </Link>
+          </li>
+          <li className="item_footer">
+            <Link className="link_footer" href="/business">
+              Log in like a business
+            </Link>
+          </li>
+          <li className="item_footer">
+            <Link className="link_footer" href="/admin">
+              Log in like an admin
+            </Link>
+          </li>
+        </ul>
+        <h5 className="text_footer">© RENT&GO 2023. All rights reserved.</h5>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
