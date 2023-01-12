86c300c95ce7b73619f1b985c2c182b41f9ac0f6
OLGAWCS <107065146+OLGAWCS@users.noreply.github.com>
2023-01-12 18:18:28 +0200

    Update vehicleInfoForm.jsx
    changed names, delete duplicate "location"

--- components/vehicleInfoForm.jsx
+++ components/vehicleInfoForm.jsx
@@ -7,63 +7,59 @@ const VehicleInfoForm = () => {
             <form className={styles.addForm}>
                 <label className={styles.infoRow}>
                     Type:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle type" />
+                    <input className={styles.itemValue} type="text" name="type" placeholder="vehicle type" />
                 </label>
                 <label className={styles.infoRow}>
                     Brand:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle brand" />
+                    <input className={styles.itemValue} type="text" name="brand" placeholder="vehicle brand" />
                 </label>
                 <label className={styles.infoRow}>
                     Model:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle model" />
+                    <input className={styles.itemValue} type="text" name="model" placeholder="vehicle model" />
                 </label>
                 <label className={styles.infoRow}>
                     Year:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle year" />
+                    <input className={styles.itemValue} type="text" name="year" placeholder="vehicle year" />
                 </label>
                 <label className={styles.infoRow}>
                     Kilometrage:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle kilometrage" />
+                    <input className={styles.itemValue} type="number" name="kilometrage" placeholder="vehicle kilometrage" />
                 </label>
                 <label className={styles.infoRow}>
                     Comfort class:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="vehicle comfort class" />
+                    <input className={styles.itemValue} type="text" name="comfort" placeholder="vehicle comfort class" />
                 </label>
                 <label className={styles.infoRow}>
                     Transmission:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="automatic / machanical" />
+                    <input className={styles.itemValue} type="text" name="transmission" placeholder="automatic / machanical" />
                 </label>
                 <label className={styles.infoRow}>
                     Power:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="electro / gas / hybrid" />
+                    <input className={styles.itemValue} type="number" name="power" placeholder="electro / gas / hybrid" />
                 </label>
                 <label className={styles.infoRow}>
                     Seats:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="number of seats" />
+                    <input className={styles.itemValue} type="number" name="seats" placeholder="number of seats" />
                 </label>
                 <label className={styles.infoRow}>
                     Photo:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="add link here" />
+                    <input className={styles.itemValue} type="text" name="photo" placeholder="add link here" />
                 </label>
                 <label className={styles.infoRow}>
                     Location:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="address" />
-                </label>
-                <label className={styles.infoRow}>
-                    Location:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="address" />
+                    <input className={styles.itemValue} type="text" name="location" placeholder="address" />
                 </label>
                 <label className={styles.infoRow}>
                     Price per km:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="price per km" />
+                    <input className={styles.itemValue} type="number" name="price" placeholder="price per km" />
                 </label>
                 <label className={styles.infoRow}>
                     Next technical inspection:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="months, year" />
+                    <input className={styles.itemValue} type="text" name="inspection" placeholder="months, year" />
                 </label>
                 <label className={styles.infoRow}>
                     IsAvailable:
-                    <input className={styles.itemValue} type="text" name="name" placeholder="yes / no" />
+                    <input className={styles.itemValue} type="text" name="IsAvailable" placeholder="yes / no" />
                 </label>
             </form>
         </>
