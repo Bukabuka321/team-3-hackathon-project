a7a55d8f03822367ded69ee045162ff8cb50a82c
OLGA DOBROVOLSKA <3250186@gmail.com>
2023-01-12 18:00:03 +0200

    added styles for description

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div>
+        <div className={styles.desc}>
           <h1>
             RENT&GO <br />
             <br />
