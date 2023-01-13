e544d5bee5984ad4024abb77b72727915b3106f7
Bukabuka321 <20uva02@gmail.com>
2023-01-13 02:50:35 +0100

    added protected route

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,23 @@
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <div>
-      <Link href={"/login"}>Login</Link>
+      <Link href={"/login"}>
+        {session ? (
+          <p className="w-20 border-2 rounded-lg border-solid bg-blue-600 text-center">
+            Sign out
+          </p>
+        ) : (
+          <p className="w-20 border-2 rounded-lg border-solid bg-blue-600 text-center">
+            Sign in
+          </p>
+        )}
+      </Link>
+      <Link href={"/protected"}>test protected page</Link>
     </div>
   );
 }
--- pages/protected.jsx
+++ pages/protected.jsx
@@ -1,19 +1,27 @@
 import { getSession, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Protected = () => {
   const { data: session, status } = useSession();
 
-  if (session === "unautheticated")
+  if (session === null || session === undefined) {
     return (
       <div>
         <h1>Protected!</h1>
+        <h2>
+          Please
+          <button className="w-24 border-2 rounded-lg border-solid bg-blue-600 text-center">
+            <Link href={"/login"}>log in</Link>
+          </button>
+        </h2>
       </div>
     );
-  return { session };
+  }
+  return <>all good</>;
 };
-
 export const getSereverSideProps = async (ctx) => {
   const session = await getSession(ctx);
+
   return {
     props: {
       session,
